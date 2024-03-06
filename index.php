f7da7e123004a347adb94b8ed081f71433ac041e
FskiTeo <teo.fiminski@epsi.fr>
2024-03-06 12:11:00 +0100

    First commit of local work

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Teo's Profile</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="w-full">
+    <div class="w-full h-screen flex flex-wrap justify-center items-center bg-gradient-to-b from-purple-100 to-purple-500">
+        <div class="rounded-md w-2/3 md:w-1/2 lg:w-1/3 bg-gray-400 flex flex-wrap justify-center py-4 bg-gradient-to-b from-white to-purple-400">
+            <div class="w-full text-center">
+                <h2 class="text-2xl font-semibold">EPSILON</h2>
+            </div>
+            <div class="w-full text-center">
+                <h4 class="text-lg">Plateforme de peer-learning<br>EPSI Lille</h4>
+            </div>
+            <div class="w-full flex flex-wrap justify-center">
+                <img src="https://pbs.twimg.com/media/FBVcjQtWYAM39Po.jpg" alt="Une image de Teo" class="rounded-full w-1/4">
+            </div>
+            <div class="w-full flex flex-wrap justify-center">
+                <h4 class="text-lg">FskiTeo</h4>
+            </div>
+            <div class="w-full flex flex-wrap justify-center">
+                <a href="upload.php" class="italic underline">Upload your file !</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
